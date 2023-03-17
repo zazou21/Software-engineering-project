753353f1cc306ebb2999f185e1cd3dd8409487e5
kiwi107 <119266066+kiwi107@users.noreply.github.com>
2023-03-17 18:30:43 +0200

    product page draft

---
+++ js/product.js
@@ -0,0 +1,14 @@
+const showsizechartbtn = document.querySelector(".showsizechart");
+const sizechartimg = document.querySelector(".sizechart");
+sizechartimg.style.display = "none";
+showsizechartbtn.onclick = toggleImg;
+
+function toggleImg() {
+    if (sizechartimg.style.display == "none") {
+        sizechartimg.style.display = "block";
+        showsizechartbtn.innerText = "hide size chart";
+    } else {
+        sizechartimg.style.display = "none";
+        showsizechartbtn.innerText = "check size chart";
+    }
+}
