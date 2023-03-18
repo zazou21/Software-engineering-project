5e5749dce5fc9d1eb0d0ea9cabe9d4f5b4976455
kiwi107 <119266066+kiwi107@users.noreply.github.com>
2023-03-18 21:22:07 +0200

    fixed sorting

--- js/sorting.js
+++ js/sorting.js
@@ -1,45 +1,50 @@
-document.getElementById("select").addEventListener("change", sortCards);
-var originalCards = document.querySelectorAll(".card");
+document.getElementById("select").addEventListener("change", sortcards);
+var originalCards = document.querySelectorAll(".card");//org cards before changing the order
 
-function sortCards() {
-    var option = document.getElementById("select").value;
+function sortcards() {
+    var option = document.getElementById("select").value;//get the value of the selected option
 
     if (option == "Low to high") {
         var cards = document.querySelectorAll(".card");
-        var sortedCards = Array.from(cards);
+        var sortedCards = Array.from(cards);//array of array-like object(cards)
+
+        //this function returns a -ve num if a < b, 0 if a = b, and a +ve num if a > b
         sortedCards.sort(function (a, b) {
-            return parseInt(a.querySelector(".price").textContent) - parseInt(b.querySelector(".price").textContent);
+            return parseInt(a.querySelector(".price").textContent) - parseInt(b.querySelector(".price").textContent);//text content of the price
         });
-        renderSortedCards(sortedCards);
-    } else if (option == "High to low") {
+
+        diplaysortedcards(sortedCards);
+    }
+    else if (option == "High to low") {
         var cards = document.querySelectorAll(".card");
         var sortedCards = Array.from(cards);
         sortedCards.sort(function (a, b) {
             return parseInt(b.querySelector(".price").textContent) - parseInt(a.querySelector(".price").textContent);
         });
-        renderSortedCards(sortedCards);
-    } else if (option == "Default") {
-        renderOriginalCards();
+        diplaysortedcards(sortedCards);
+    }
+    else if (option == "Default") {
+        displayoriginalcards();
     }
 }
 
-function renderSortedCards(sortedCards) {
+function diplaysortedcards(sortedCards) {
     var container = document.querySelector(".container");
-    var sortedContainer = document.createElement("div");
-    sortedContainer.className = "row sorted-cards";
+    var sortedContainer = document.createElement("div");//creating a new div for the sorted cards
+    sortedContainer.className = "row gy-3";//adding bootstrap classes
 
     for (var i = 0; i < sortedCards.length; i++) {
-        var col = document.createElement("div");
-        col.className = "col-lg-3 col-md-4 col-sm-6 mb-4";
-        col.appendChild(sortedCards[i]);
-        sortedContainer.appendChild(col);
+        var col = document.createElement("div");//creating a new div for each card
+        col.className = "col-12 col-md-6 col-lg-4 col-xl-3";//adding bootstrap classes
+        col.appendChild(sortedCards[i]);//adding card to the div
+        sortedContainer.appendChild(col);//adding the div to the container
     }
 
-    container.innerHTML = "";
-    container.appendChild(sortedContainer);
+    container.innerHTML = "";//removing the original cards
+    container.appendChild(sortedContainer);//adding the sorted cards
 }
 
-function renderOriginalCards() {
+function displayoriginalcards() {
     var container = document.querySelector(".container");
     var originalContainer = document.createElement("div");
     originalContainer.className = "row original-cards";
