361af0ac2c1851577a6c2b1eb3b1c54b4b8aeb09
kiwi107 <119266066+kiwi107@users.noreply.github.com>
2023-03-19 03:44:55 +0200

    change color btns

--- js/product.js
+++ js/product.js
@@ -1,3 +1,5 @@
+//make the size chart button toggle the size chart image
+
 const showsizechartbtn = document.querySelector(".showsizechart");
 const sizechartimg = document.querySelector(".sizechart");
 sizechartimg.style.display = "none";
@@ -12,3 +14,25 @@ function toggleImg() {
         showsizechartbtn.innerText = "check size chart";
     }
 }
+
+//make the smallimg replace the bigimg when clicked
+//1 using btns selection
+var smallimgs = document.querySelectorAll(".smallimg");
+var bigimg = document.querySelector(".bigimg");
+var radiobtn = document.querySelectorAll(".radiobtn");
+for (let j = 0; j < radiobtn.length; j++) {
+    radiobtn[j].onclick = function () {
+        bigimg.src = smallimgs[j].src;
+    }
+}
+
+//2 using img selection
+for (let i = 0; i < smallimgs.length; i++) {
+
+    smallimgs[i].onclick = function () {
+        bigimg.src = smallimgs[i].src;
+        radiobtn[i].checked = true;
+    }
+}
+
+
