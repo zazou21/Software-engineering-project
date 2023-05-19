ebd0e1958cb09ef4e97649d3ada05e7b8a285f86
karim sherif <119266066+kiwi107@users.noreply.github.com>
2023-05-19 18:14:30 +0300

    product page from database and customizable size and color

--- SoftwareProjDjango/software/Home/views.py
+++ SoftwareProjDjango/software/Home/views.py
@@ -6,8 +6,9 @@ from django.contrib.auth import login,logout,authenticate
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import registration
-from .models import *
 from django.views.generic import ListView
+from .models import childrenproduct,Cart
+
 # Create your views here.
 
 
@@ -29,10 +30,24 @@ def aboutus(request):
 
 
 def cart(request):
-    cartobjects=Cart.objects.filter(user=request.user)
+    return render(request,'Home/cart.html')
+
+def newcart(request):
+    return render(request,'Home/newcart.html')
+
+class cartListView(ListView):
+    model=Cart
+    # context_object_name='cart'
+    template_name = 'Home/newcart.html'
+
+    # def get_queryset(self):
+    #     return Cart.objects.all()
 
 
-    return render(request,'Home/cart.html',{'cartobjects':cartobjects})
+
+
+
+
 
 def Myaccount(request):
     if (request.method=="POST") and (not request.user.is_authenticated) :
@@ -45,31 +60,11 @@ def Myaccount(request):
            return redirect('home')
        
         else:
-           messages.success(request,"Login failed")
+           messages.success(request,"log in failed")
            return redirect('Myaccount')
     else:
         
         return render(request, 'Home/myaccount.html')
-    
-
-class childrenproductListView(ListView):
-    model=childrenproduct
-    context_object_name='children'
-
-def h(request):
-    return render(request,'Home/h.html')
-
-def product_view(request,child_name,child_price,child_img,child_productID):
-    product = get_object_or_404(childrenproduct, productID=child_productID)
-    context = {
-        'child_name': child_name,
-        'child_price': child_price,
-        'child_img': child_img,
-        'product': product,
-        'colors': product.product_attributes.first().colors.all()
-       
-    }
-    return render(request, 'Home/product.html', context)
     
     
 
@@ -104,8 +99,26 @@ def Girl(request):
 def children(request):
     return render(request,'Home/children.html')
 
-def product(request):
-    return render(request,'Home/product.html')
+
+class childrenproductListView(ListView):
+    model=childrenproduct
+    context_object_name='children'
+
+def h(request):
+    return render(request,'Home/h.html')
+
+def product_view(request,child_name,child_price,child_img,child_productID):
+    product = get_object_or_404(childrenproduct, productID=child_productID)
+    context = {
+        'child_name': child_name,
+        'child_price': child_price,
+        'child_img': child_img,
+        'product': product,
+        'colors': product.product_attributes.first().colors.all(),
+        'sizes': product.product_attributes.first().sizes.all(),
+    }
+    return render(request, 'Home/product.html', context)
+
 
 def baby(request):
     return render(request, 'Home/baby.html')
@@ -125,20 +138,6 @@ def register_user(request):
         Form=registration()   
 
     return render(request,'Home/register.html',{'Form':Form})
-
-def addToCart(request):
-    if request.method=='POST':
-        if request.user.is_authenticated:
-            prod_id=int(request.POST.get('product_id'))
-            Cart.objects.create(user=request.user,product_id=prod_id)
-    
-    return redirect('home')
-
-
-
-
-       
-
         
         
         
